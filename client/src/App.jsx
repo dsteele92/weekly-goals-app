341249d170ff5699c6736d3dfec96b954c85e8a4
Dalton Steele <dsteele92@gmail.com>
2022-06-24 23:31:36 -0700

    Organized client side ready to begin

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.scss';
+
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+	return <div className='App'>YOOOOOO</div>;
+};
+
+export default App;
