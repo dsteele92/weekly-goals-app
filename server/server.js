b65fc581d56c70991c69fc0b31118af20a468f32
Dalton Steele <dsteele92@gmail.com>
2022-06-24 22:46:02 -0700

    After setting up server side to connect to mongo db using mongoose

--- server/server.js
+++ server/server.js
@@ -2,18 +2,30 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
+// console.log(process.env)
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+const mongoose = require('mongoose');
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+
+// app.listen(port, () => {
+//   // perform a database connection when server starts
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
+
+//   });
+//   console.log(`Server is running on port: ${port}`);
+// });
+
+
